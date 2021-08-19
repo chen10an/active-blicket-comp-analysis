5452d669064d3f24b4b16a4553455960d8794030
Chentian Jiang <chentian.jiang@gmail.com>
2021-08-19 21:32:38 +0100

    make input option for choosing which experiment version to process

--- py_scripts/v2_save_good_endings.py
+++ py_scripts/v2_save_good_endings.py
@@ -7,14 +7,18 @@ import pandas as pd
 import jmespath
 
 DATA_DIR_PATH = '../ignore/data/'
-SAVE_PATH = '../ignore/output/v2/good_endings.json'
+
+option_dex = int(input("0 for 200-mturk; 1 for 20x-mturk-micro"))
+chunks_filename = ['chunks_200-mturk.json', 'chunks_20x-mturk-micro.json'][option_dex]
+ids_filename = ['d_mturk_worker_ids_200-mturk.tsv', 'd_mturk_worker_ids_20x-mturk-micro.tsv'][option_dex]
+save_path = ['../ignore/output/v2/good_endings.json', '../ignore/output/v2/good_endings_micro.json'][option_dex]
 
 ## %%
 # Get a list of dicts (chunks) and df of MTurk worker IDs for v2
-with open(os.path.join(DATA_DIR_PATH, 'chunks_200-mturk.json')) as f:
+with open(os.path.join(DATA_DIR_PATH, chunks_filename)) as f:
     data_list = json.load(f)
 
-with open(os.path.join(DATA_DIR_PATH, 'd_mturk_worker_ids_200-mturk.tsv')) as f:
+with open(os.path.join(DATA_DIR_PATH, ids_filename)) as f:
     id_df = pd.read_csv(f, sep='\t')
 
 print(f"Number of rows in the raw participant ID file: {id_df.shape[0]}")
@@ -40,11 +44,12 @@ for chunk in data_list:
     if filtered_df.session_id.str.contains(chunk['sessionId']).any():
         filtered_chunks.append(chunk)
 
+print(f"Number of good chunks that match to the filtered participant IDs: {len(filtered_chunks)}")
 ## %%
 # finally get just the good ending chunks, where participants did not reach a trouble ending by failing comprehension or captcha checks
 good_endings = jmespath.search("[?seq_key=='End'] | [?!is_trouble]", filtered_chunks)
 
 ##%%
-with open(SAVE_PATH, 'w') as f:
+with open(save_path, 'w') as f:
     json.dump(good_endings, f)
-print(f"Saved v2 good ending chunks to {SAVE_PATH}!")
+print(f"Saved v2 good ending chunks to {save_path}!")
