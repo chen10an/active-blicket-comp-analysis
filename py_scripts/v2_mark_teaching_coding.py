de6d76eaf5be87923a4916b74eb0c9ec003ad9bc
Chentian Jiang <chentian.jiang@gmail.com>
2021-08-20 22:51:21 +0100

    start on marking and visualizing micro study data

--- py_scripts/v2_mark_teaching_coding.py
+++ py_scripts/v2_mark_teaching_coding.py
@@ -4,27 +4,47 @@
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Qt5Agg')
 
 BONUS = 0.16  # possible bonus per set/level of teaching questions
 OUTPUT_DIR_PATH = '../ignore/output/v2/'
 CODING_DIR_PATH = '../ignore/output/v2/coding/'
 
-SAVE_PATH = '../ignore/output/v2/coding/bonus_teaching_coding_20x-mturk_pilot.csv'
+option_dex = int(input("0 for 200-mturk pilot; 1 for 20x-mturk-micro unfinished coding"))
+truth_filename = ['quiz_teaching.csv', 'micro_teaching.csv'][option_dex]
+coded_filename = ['done_teaching_coding_20x-mturk_pilot.csv', 'doing_teaching_coding_micro.csv'][option_dex]
+save_path = ['../ignore/output/v2/coding/bonus_teaching_coding_20x-mturk_pilot.csv', '../ignore/output/v2/coding/bonus_teaching_coding_micro.csv'][option_dex]
 
 # load coded teaching examples
-coded_df = pd.read_csv(os.path.join(CODING_DIR_PATH, 'done_teaching_coding_20x-mturk_pilot.csv'))
+coded_df = pd.read_csv(os.path.join(CODING_DIR_PATH, coded_filename))
 
 # load teaching examples with their associated ground truth information (i.e., session_id, training/form, level)
-truth_df = pd.read_csv(os.path.join(OUTPUT_DIR_PATH, 'quiz_teaching.csv'))
+truth_df = pd.read_csv(os.path.join(OUTPUT_DIR_PATH, truth_filename))
 
 ##
 # label every row with a shorthand name for the ground truth form
 
-# for level 1, the shorthand name is just the same as training without the level '1' part
-truth_df.loc[truth_df.level == 1, 'true_short_form'] = truth_df.loc[truth_df.level == 1].training.apply(lambda x: x.replace('1', ''))
+if option_dex == 0:
+    # for level 1, the shorthand name is just the same as training without the level '1' part
+    truth_df.loc[truth_df.level == 1, 'true_short_form'] = truth_df.loc[truth_df.level == 1].training.apply(lambda x: x.replace('1', ''))
 
-# the level 2 form is always 'c' (deterministic conjunctive)
-truth_df.loc[truth_df.level == 2, 'true_short_form'] = 'c'
+    # the level 2 form is always 'c' (deterministic conjunctive)
+    truth_df.loc[truth_df.level == 2, 'true_short_form'] = 'c'
+    
+elif option_dex == 1:
+    long_to_shorthand_form = {
+        'disj': 'd',
+        'noisy_conj': 'nc',
+        'conj3': 'cc',
+        'noisy_disj': 'nd',
+        'conj': 'c',
+        'noisy_conj3': 'ncc',
+        'participant': 'p'
+        }
+
+    truth_df['true_short_form'] = truth_df.apply(lambda row: long_to_shorthand_form[row.true_form], axis=1)
 
 ##
 # join on hash id, getting rid of the randomly generated rows
@@ -44,8 +64,14 @@ code_to_shorthand_form = {
 
 def convert_to_shorthand_form(row):
     if pd.isnull(row.form):
-        # if the coded form is nan, just return nan again
-        return np.nan
+        # if the coded form is nan, check if it has been labeled inside/outside (exclusively)
+        # XOR:
+        assert((row.is_inside or row.is_outside) and (row.is_inside != row.is_outside))
+        
+        if row.is_inside:
+            return 'inside'
+        elif row.is_outside:
+            return 'outside'
     
     return code_to_shorthand_form[(row.form, row.is_noisy)]
 
@@ -60,7 +86,68 @@ merged_df['BonusAmount'] = 0.0
 merged_df.loc[merged_df.is_correct, 'BonusAmount'] = BONUS
 
 # view
-merged_df[['training', 'true_short_form', 'coded_short_form', 'is_correct', 'BonusAmount']]
+merged_df[['true_short_form', 'coded_short_form', 'is_correct', 'BonusAmount']]
+
+##
+# plot
+# merged_df.groupby('true_short_form').is_correct.mean().plot.bar()
+# plt.show()
+
+##
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+labels = ['d', 'nd', 'c', 'nc', 'cc', 'ncc', 'p', 'inside', 'outside'] 
+cm = confusion_matrix(y_true=merged_df.true_short_form, y_pred=merged_df.coded_short_form, labels=labels)
+ConfusionMatrixDisplay(cm, display_labels=labels).plot()
+plt.show()
+
+##
+merged_df.loc[(merged_df.true_short_form == 'ncc') & (merged_df.coded_short_form == 'ncc'), ['condition'] + [f'ex_{i}_x' for i in range(5)]]
+
+##
+ordered_forms = ['d', 'nd', 'c', 'nc', 'cc', 'ncc']
+
+# whether a form has been misclassified by exactly diff indices in ordered_forms
+def has_diff(row, diff):
+    if row.true_short_form == 'p':
+        return False
+    
+    true_dex = ordered_forms.index(row.true_short_form)
+
+    # extremities
+    if true_dex+diff < 0 or true_dex+diff >= len(ordered_forms):
+        return False
+    
+    return row.coded_short_form == ordered_forms[true_dex+diff]
+
+merged_df['wrong_prev'] = merged_df.apply(lambda row: has_diff(row, -1), axis=1)
+
+merged_df['wrong_next'] = merged_df.apply(lambda row: has_diff(row, 1), axis=1)
+
+##
+wrong_df = merged_df[~merged_df.is_correct]
+
+# misclassifications that are not captured by next/prev/inside/outside
+wrong_other_mask = (wrong_df[['wrong_next', 'wrong_prev', 'is_inside', 'is_outside']].apply(lambda row: row.sum(), axis=1) == 0)
+
+wrong_df.loc[:, 'wrong_other'] = False
+wrong_df.loc[wrong_other_mask, 'wrong_other'] = True
+
+# check that only one type of wrong applies at a time
+assert(all(wrong_df[['wrong_next', 'wrong_prev', 'is_inside', 'is_outside', 'wrong_other']].apply(lambda row: row.sum(), axis=1) == 1))
+
+##
+# set order for plotting below
+from pandas.api.types import CategoricalDtype
+cat_type = CategoricalDtype(categories=ordered_forms + ['p'], ordered=True)
+wrong_df['true_short_form'] = wrong_df.true_short_form.astype(cat_type)
+
+##
+wrong_df.groupby('true_short_form')[['wrong_next', 'wrong_prev', 'is_inside', 'is_outside', 'wrong_other']].sum().plot.bar(stacked=True)
+plt.show()
+
+##
+# any order effects?
 
 ##
 # filter out columns not needed for mturk bonusing
@@ -71,8 +158,8 @@ bonus_df = bonus_df.groupby('session_id').sum()
 
 ##
 # save
-bonus_df.to_csv(SAVE_PATH)
-print(f"Saved the bonus df to {SAVE_PATH}!")
+# bonus_df.to_csv(save_path)
+# print(f"Saved the bonus df to {save_path}!")
 
 ##
 # TODO: take a look at how the randomly generated rows were coded!
