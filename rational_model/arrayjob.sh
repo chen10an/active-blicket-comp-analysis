bd294343162c01a7867a91bf9a2e1dfb1de0856e
Chentian Jiang <chentian.jiang@gmail.com>
2022-04-19 12:46:11 +0100

    increase ram by a bit

--- rational_model/arrayjob.sh
+++ rational_model/arrayjob.sh
@@ -42,7 +42,7 @@
 # #SBATCH --gres=gpu:1
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
-#SBATCH --mem=16000
+#SBATCH --mem=20000
 
 # Number of CPUs to use. Check `cluster-status` for node configurations
 #SBATCH --cpus-per-task=1
