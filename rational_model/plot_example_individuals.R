be6f5f9ae30dbaf739986c50b05ed555004c4a23
Chentian Jiang <25162779+chen10an@users.noreply.github.com>
2023-09-01 16:09:46 +0100

    update plotting scripts

---
+++ rational_model/plot_example_individuals.R
@@ -0,0 +1,57 @@
+library(data.table)
+library(magrittr)
+source("../plots_and_stats/plotting_helperfuns.R")
+source("helperfuns.R")
+
+# get interventions
+interventionsDT <- fread(file = '../ignore/output/v2/interventions2.csv')
+interventionsDT$timestamp <- as.POSIXct(interventionsDT$timestamp/1000, origin="1970-01-01") 
+interventionsDT[, nthIntervention := rowid(session_id)]
+
+block_id_cols <- c("id_0", "id_1", "id_2", "id_3", "id_4", "id_5")
+
+plotSess <- function(sess) {
+  sessDT <- interventionsDT[session_id == sess]
+  sessDT <- melt(sessDT, measure.vars = block_id_cols, variable.name = "block_id", value.name = "block_state")
+  levels(sessDT$block_id) <- list("Blicket 1"="id_0", "Blicket 2"="id_1", "Blicket 3"="id_2", "Non-Blicket 1"="id_3", "Non-Blicket 2"="id_4", "Non-Blicket 3"="id_5")
+  sessDT[outcome == FALSE, outcome_char := "Nothing"]
+  sessDT[outcome == TRUE, outcome_char := "Activation"]
+  ggplot(data = sessDT, aes(x = nthIntervention, y = block_state, fill = outcome_char)) +
+    geom_col(width = 0.1) +
+    scale_fill_brewer(palette = "Dark2") +
+    facet_grid("block_id ~ .", switch = "y") +
+    scale_x_discrete(limits=1:max(sessDT$nthIntervention)) +
+    ylab(NULL) +
+    xlab("Intervention") +
+    labs(fill = "Blicket Machine\nResponse") +
+    theme_mine() +
+    theme(panel.spacing = unit(0, "lines"),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          panel.border = element_blank(),
+          panel.grid.major.y = element_blank(),
+          panel.grid.major.x = element_line( size=.3, color="light gray"),
+          strip.text.y.left = element_text(angle = 0, hjust = 0),
+          legend.position = "top",
+          plot.title = element_text(size = 10, face = "bold", margin = margin(0, 0, 5, 0), hjust = 0.05),
+          legend.key.size = unit(0.5, 'cm'),
+          plot.margin = margin(0, 0, 0.3, 0, unit = "cm"))
+}
+
+# plotSess("NUC1Zp1CqK4h3vT4okD2FAkSxWMS9gVb")
+
+legend <- get_legend(plotSess("NUC1Zp1CqK4h3vT4okD2FAkSxWMS9gVb"))
+
+hbmP <- plotSess("NUC1Zp1CqK4h3vT4okD2FAkSxWMS9gVb") + ggtitle("HBM Participant") + theme(legend.position = "none")
+
+noTransferP <- plotSess("bh9MvmNflP9cVw63DvOMSx9qFBTWRkY0") + ggtitle("No Transfer Participant") + theme(legend.position = "none")
+
+structOnlyP <- plotSess("ye7cOoHSbasWoqnJrdnCf3adcQ5IlM4C") + ggtitle("Structure-Only EIG Participant") + theme(legend.position = "none")
+
+randP <- plotSess("kr7cXcBBAXw8qObaZeOrb9l9FSuds2bQ") + ggtitle("Random Participant") + theme(legend.position = "none")
+
+allP <- plot_grid(hbmP, noTransferP, structOnlyP, randP, legend, ncol = 1, rel_heights = c(rep(0.93/4, 4), 0.07))
+
+plot_grid(hbmP, noTransferP, structOnlyP, randP, legend, ncol = 1, rel_heights = c(rep(0.98/4, 4), 0.02))
+
+save_plot(filename = "../../../Dropbox/drafts/2021-Feb_active_overhypo_modeling/imgs/example_ind.pdf", plot = allP, base_height = NULL, base_width = 5, base_asp = 0.7)
--- rational_model/plot_fit_individual.R
+++ rational_model/plot_fit_individual.R
@@ -1,6 +1,5 @@
 library(data.table)
 library(magrittr)
-library(ggplot2)
 source("../plots_and_stats/plotting_helperfuns.R")
 source("helperfuns.R")
 
@@ -11,34 +10,54 @@ mainFits <- fits
 mainMean <- lapply(mainFits, function(sess) mean(unlist(lapply(sess, function(fold) fold$testMarginalMeanPredLik))))
 mainMeanDT <- data.table(session_id = names(mainMean), mean = mainMean)
 mainMeanDT[, model := "HBM"]
+mainTrainFits <- lapply(mainFits, function(sess) rbindlist(lapply(sess, function(fold) fold$bestTrainFit)))
+mainTrainFitDT <- rbindlist(mainTrainFits, idcol = "session_id")
+mainTrainFitDT[, model := "HBM"]
 
 load("cache/2022-04-18_noP1/fits_allTestPredLikPerInt_useStructEIG=FALSE_individuals/all.RData")
 noP1Fits <- fits
 # mean testMarginalMeanPredLik over all folds for each participant
 noP1Mean <- lapply(noP1Fits, function(sess) mean(unlist(lapply(sess, function(fold) fold$testMarginalMeanPredLik))))
 noP1MeanDT <- data.table(session_id = names(noP1Mean), mean = noP1Mean)
-noP1MeanDT[, model := "No Transfer"]
+noP1MeanDT[, model := "No-Transfer"]
+noP1TrainFits <- lapply(noP1Fits, function(sess) rbindlist(lapply(sess, function(fold) fold$bestTrainFit)))
+noP1TrainFitDT <- rbindlist(noP1TrainFits, idcol = "session_id")
+noP1TrainFitDT[, model := "No-Transfer"]
 
 load("cache/2022-04-18_main/fits_allTestPredLikPerInt_useStructEIG=TRUE_individuals/all.RData")
 noInfoFits <- fits
 # mean testMarginalMeanPredLik over all folds for each participant
 noInfoMean <- lapply(noInfoFits, function(sess) mean(unlist(lapply(sess, function(fold) fold$testMarginalMeanPredLik))))
 noInfoMeanDT <- data.table(session_id = names(noInfoMean), mean = noInfoMean)
-noInfoMeanDT[, model := "No Form EIG"]
+noInfoMeanDT[, model := "Structure-Only-EIG"]
+noInfoTrainFits <- lapply(noInfoFits, function(sess) rbindlist(lapply(sess, function(fold) fold$bestTrainFit)))
+noInfoTrainFitDT <- rbindlist(noInfoTrainFits, idcol = "session_id")
+noInfoTrainFitDT[, model := "Structure-Only-EIG"]
 
 load("cache/2022-04-08_noSig-1/fits_allTestPredLikPerInt_useStructEIG=FALSE_individuals/all.RData")
 noSig1Fits <- fits
 # mean testMarginalMeanPredLik over all folds for each participant
 noSig1Mean <- lapply(noSig1Fits, function(sess) mean(unlist(lapply(sess, function(fold) fold$testMarginalMeanPredLik))))
 noSig1MeanDT <- data.table(session_id = names(noSig1Mean), mean = noSig1Mean)
-noSig1MeanDT[, model := "No Form Space"]
+noSig1MeanDT[, model := "Fixed-Form"]
+noSig1TrainFits <- lapply(noSig1Fits, function(sess) rbindlist(lapply(sess, function(fold) fold$bestTrainFit)))
+noSig1TrainFitDT <- rbindlist(noSig1TrainFits, idcol = "session_id")
+noSig1TrainFitDT[, model := "Fixed-Form"]
 
 randomDT <- data.table(session_id = mainMeanDT$session_id, mean = 1/64, model = "Random")
   
-compareDT <- rbind(mainMeanDT, noP1MeanDT, noInfoMeanDT, noSig1MeanDT, randomDT)
+# OBS: random model comes first so it gets picked when another model (i.e., noSig1) has the same predictive likelihood as the random level
+compareDT <- rbind(randomDT, mainMeanDT, noP1MeanDT, noInfoMeanDT, noSig1MeanDT)
+paramDT <- rbind(mainTrainFitDT, noP1TrainFitDT, noInfoTrainFitDT, noSig1TrainFitDT)
 
-bestDT <- compareDT[, .(bestModel = .SD$model[which.max(mean)]), by=session_id]
-bestDT[, bestModel := factor(bestModel, levels=c("HBM", "No Transfer", "No Form Space", "No Form EIG", "Random"))]
+# compute posterior probabilities of each model p(model | sess), assuming a uniform prior
+compareDT[, mean := unlist(mean)]  # list data struct seems to be remaining from fits
+compareDT[, posterior := mean/sum(mean), by = session_id]
+
+compareDT[mean == 1/64]$model %>% unique()
+
+bestDT <- compareDT[, .(bestModel = .SD$model[which.max(mean)], bestPosterior = .SD$posterior[which.max(mean)]), by=session_id]
+bestDT[, bestModel := factor(bestModel, levels=c("HBM", "No-Transfer", "Structure-Only-EIG", "Fixed-Form", "Random"))]
 
 p <- ggplot(bestDT, aes(x=bestModel, fill=bestModel)) +
   scale_fill_manual(values=c(brewer.pal(n = 8, "Dark2")[1:4], myGray)) +
@@ -50,5 +69,5 @@ p <- ggplot(bestDT, aes(x=bestModel, fill=bestModel)) +
   theme_mine() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 print(p)
-# save_plot(filename = "../../../Dropbox/drafts/2021-Feb_active_overhypo_modeling/imgs/model_comparison_ind.pdf", plot = p, base_height = NULL, base_width = 2.5, base_asp = 0.7)
 
+# save_plot(filename = "../../../Dropbox/drafts/2021-Feb_active_overhypo_modeling/imgs/model_comparison_ind.pdf", plot = p, base_height = NULL, base_width = 2.5, base_asp = 0.7)
